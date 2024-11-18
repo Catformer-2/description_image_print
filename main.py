9e44f84d5102038c14c171f39cb1eb7088176b1b
CatFormer <92211148+Catformer-2@users.noreply.github.com>
2024-11-18 15:01:21 +0300

    Перенёс код

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import json
 from PIL import Image
 
-with open("waypoint.txt", "r") as f:
-    waypoints = json.load(f)
-
-initial_x = waypoints[0]["pos"]["x"]
-initial_z = waypoints[0]["pos"]["z"]
-
-
+sel_character = ":::::"
 image = Image.open("input.png")
 image = image.convert("RGBA")
 width, height = image.size
@@ -15,6 +9,7 @@ pixels = image.load()
 new_waypoints = []
 
 for y in range(height):
+    new_waypoints.append("\n")
     for x in range(width):
         pixel = pixels[x, y]
         if len(pixel) == 3:
@@ -23,24 +18,9 @@ for y in range(height):
         else:
             r, g, b, a = pixel
 
-        argb_color = (a << 24) | (r << 16) | (g << 8) | b
-        decimal_color = argb_color if argb_color >= 0 else argb_color + 2**32
-
-        new_waypoint = {
-            "time": waypoints[0]["time"],
-            "name": f"{waypoints[0]['name']}_{y * width + x}",
-            "pos": {
-                "x": initial_x + x,
-                "y": waypoints[0]["pos"]["y"],
-                "z": initial_z - (height - 1 - y)
-            },
-            "color": decimal_color,
-            "icon_index": waypoints[0]["icon_index"],
-            "can_position_float": waypoints[0]["can_position_float"],
-            "type": waypoints[0]["type"]
-        }
-
+        new_waypoint = rgb_to_hex(r, g, b) + sel_character
         new_waypoints.append(new_waypoint)
 
-with open("output_waypoints.txt", "w") as f:
-    json.dump(new_waypoints, f, separators=(",", ":"))
+with open('Description.txt', 'w') as f:
+    for line in new_waypoints:
+        f.write(f"{line}")
